85160de49de7864dbe283af4b1a402e6c837b7a3
EugeneIm <immer277@gmail.com>
2022-02-17 10:18:43 -0800

    basic functions written

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,51 @@
-//Imports for later
+//Imports for Gulp
+const { src, dest, watch, series } = require('gulp')
+
+//Module imports
+const htmlmin = require('gulp-htmlmin')
+const browserSync = require('browser-sync').create()
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps')
 
 //Hello task
 
-/*
-    function gulpTemplate(cb){
-        console.log("gulp template")
-        cb();
-    }
-    exports.template = gulpTemplate;
-    run the function as "template" and not "gulpTemplate"
-*/
+function watchTask(){
+    browserSync.init({
+        server:{
+            baseDir:'./dev'
+        }
+    });
+    watch('./dev/scss/*/**', sassTask);
+    watch('./dev/scripts/**/*/*.js').on('change' ,browserSync.reload);
+    watch('./dev/scripts/**/*/*.html').on('change' ,browserSync.reload);
+}
+
+function sassTask(){
+    return src('./dev/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest('./dev/styles'))
+    .pipe(browserSync.stream());
+}
 
 function helloTask(done){
     console.log("gulp test run")
     done();
 }
 
+//HTML Task
+function htmlTask(){
+    return src('./dev/**/*.html')
+    .pipe(htmlmin({collapseWhitespace:true}))
+    .pipe(dest('build'))
+}
+
+
+//task exports
 exports.hello = helloTask;
+exports.html = htmlTask;
+exports.sass = sassTask;
+
+//default tasks
+exports.default = series(sassTask, watchTask)
